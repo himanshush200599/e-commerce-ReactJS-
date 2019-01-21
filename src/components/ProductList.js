57afc363dcf27cd0986f2d2a3eb9518d79164527
himanshush200599 <himanshush214@gmail.com>
2019-01-21 05:59:14 -0500

    set up products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Product from "./Product";
+import Title from "./Title";
 class ProductList extends Component {
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row" />
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
