15e237eeb55e6efe782b1be4884b4f6e8a9f480a
himanshush200599 <himanshush214@gmail.com>
2019-02-17 17:28:38 -0500

    Page not found done

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
 class Default extends Component {
   render() {
-    return <div>Page not found 404</div>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>Error</h1>
+            <h2>page not found</h2>
+            <h3>
+              Requested URL{" "}
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>{" "}
+              was not found
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Default;
